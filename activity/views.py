37c7af32a7f3873488db0370c6f03210a9050bfe
Tirth <tirth.gada@trilogy.com>
2025-05-19 23:30:41 +0530

    Added views

--- activity/views.py
+++ activity/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import Todo
 from .forms import TodoForm
 from django.contrib import messages
 
 # Create your views here.
+def detail(request, item_id):
+    item = get_object_or_404(Todo, id=item_id)
+    return render(request, 'detail.html', {'item': item, 'title': 'Todo Details'})
+
+
+
+def edit(request, item_id):
+    item = get_object_or_404(Todo, id=item_id)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Todo updated successfully!")
+            return redirect('todo')
+    else:
+        form = TodoForm(instance=item)
+    return render(request, 'edit.html', {'form': form, 'title': 'Edit Todo'})
+
+
 def index(request):
     item_list = Todo.objects.order_by("date")
     if request.method == "POST":
