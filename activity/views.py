7a4bbb5bfe182b944d796da7fbce657412b2cd28
TirthGada <118129263+TirthGada@users.noreply.github.com>
2025-05-20 00:08:58 +0530

    Model update

--- activity/views.py
+++ activity/views.py
@@ -9,6 +9,13 @@ def detail(request, item_id):
     item = get_object_or_404(Todo, id=item_id)
     return render(request, 'detail.html', {'item': item, 'title': 'Todo Details'})
 
+def latest(request):
+    latest_item = Todo.objects.order_by('-date').first()
+    context = {
+        'item': latest_item,
+        'title': 'Latest Todo',
+    }
+    return render(request, 'latest.html', context)
 
 
 def edit(request, item_id):
